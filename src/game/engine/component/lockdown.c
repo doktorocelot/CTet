60800d2fd6ae5438f87e2dd883f4f62d65b4065e
Dr Ocelot <19556715+doktorocelot@users.noreply.github.com>
2023-07-27 12:13:28 -0400

    refine lockdown

--- src/game/engine/component/lockdown.c
+++ src/game/engine/component/lockdown.c
@@ -1,6 +1,11 @@
 #include "lockdown.h"
 #include "../../../math/quick-math.h"
 
+static void lockdown_resetVars(Lockdown *lockdown);
+
+static bool lockdown_shouldLock(const Lockdown *lockdown, ActivePiece *activePiece);
+
+
 bool lockdown_tick(Lockdown *lockdown, ActivePiece *activePiece, float deltaTime) {
 
     if (activePiece_collidesWith(activePiece, (Point) {0, -1})) {
@@ -11,21 +16,36 @@ bool lockdown_tick(Lockdown *lockdown, ActivePiece *activePiece, float deltaTime
     lockdown->pieceLowestY = MIN(lockdown->pieceLowestY, activePiece->pos.y);
 
     if (pieceLastLowestY > lockdown->pieceLowestY) {
-        lockdown->lockDelayAcc = 0;
+        lockdown_resetVars(lockdown);
     }
 
-    if (lockdown->lockDelayAcc >= LOCK_DELAY) {
-        lockdown->lockDelayAcc -= LOCK_DELAY;
+    if (lockdown_shouldLock(lockdown, activePiece)) {
         return true;
     }
     return false;
 }
 
 void lockdown_onPieceSpawn(Lockdown *lockdown, ActivePiece *activePiece) {
-    lockdown->lockDelayAcc = 0;
+    lockdown_resetVars(lockdown);
     lockdown->pieceLowestY = activePiece->pos.y;
 }
 
 void lockdown_onPieceManipulate(Lockdown *lockdown) {
+    lockdown->pieceManipulations++;
+    if (lockdown->pieceManipulations > MANIPULATION_LIMIT) {
+        lockdown->shouldForceLockPiece = true;
+    } else {
+        lockdown->lockDelayAcc = 0;
+    }
+}
+
+void lockdown_resetVars(Lockdown *lockdown) {
     lockdown->lockDelayAcc = 0;
+    lockdown->pieceManipulations = 0;
+    lockdown->shouldForceLockPiece = false;
+}
+
+bool lockdown_shouldLock(const Lockdown *lockdown, ActivePiece *activePiece) {
+    return (lockdown->lockDelayAcc >= LOCK_DELAY || lockdown->shouldForceLockPiece)
+           && activePiece_collidesWith(activePiece, (Point) {0, -1});
 }
--- src/game/engine/component/lockdown.h
+++ src/game/engine/component/lockdown.h
@@ -4,10 +4,13 @@
 #include "active-piece.h"
 
 #define LOCK_DELAY 500
+#define MANIPULATION_LIMIT 15
 
 typedef struct {
     float lockDelayAcc;
     int pieceLowestY;
+    int pieceManipulations;
+    bool shouldForceLockPiece;
 } Lockdown;
 
 bool lockdown_tick(Lockdown *lockdown, ActivePiece *activePiece, float deltaTime);
